0447e72d814f7bd8ac55dc3f195406a93d7f7764
Guillaume Ebersohl <guigui1991@live.fr>
2025-04-22 10:21:40 +0200

    build: updated build tree structure

--- cub3d.mk
+++ cub3d.mk
@@ -1,7 +1,12 @@
-override SRCSDIR	:= srcs_mandatory/
-override SRCS		= $(addprefix $(SRCSDIR), $(SRC))
+override SRCSDIR	:= srcs/
+override SRCS		= $(addprefix $(SRCSDIR)$(MANDATORYDIR), $(SRC))
+override SRCSBONUS	= $(addprefix $(SRCSDIR)$(BONUSDIR), $(SRCBONUS))
+
+override MANDATORYDIR	:= mandatory/
+override BONUSDIR		:= bonus/
 
 override UTILSDIR	:= utils/
+override PARSINGDIR	:= parsing/
 
 SRC	+= $(addsuffix .c, $(MAIN))
 
@@ -11,4 +16,9 @@ override MAIN			:= \
 SRC += $(addprefix $(UTILSDIR), $(addsuffix .c, $(UTILSSRC)))
 
 override UTILSSRC		:= \
-	test \
+	utils \
+
+SRC += $(addprefix $(PARSINGDIR), $(addsuffix .c, $(PARSINGSRC)))
+
+override PARSINGSRC		:= \
+	check_param_validity \
