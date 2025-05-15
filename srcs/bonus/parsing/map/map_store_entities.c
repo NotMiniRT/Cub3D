81eefa186846c4b8be4acaf3a21e93049dd1a8a7
Guillaume Ebersohl <guigui1991@live.fr>
2025-05-15 19:09:25 +0200

    feat: updated bonus characters storage and check to match discussed choices

--- srcs/bonus/parsing/map/map_store_entities.c
+++ srcs/bonus/parsing/map/map_store_entities.c
@@ -1,61 +1,29 @@
 #include "map_validation.h"
+#include "parsing.h"
 
-void	store_collectibles(t_map_data map_data, t_infos *infos)
+void	store_door(t_map_data map_data, int i, int j, t_infos *infos)
 {
-	int	i;
-	int	j;
-	int	count;
+	int		count;
+	bool	horizontal;
+	bool	vertical;
 
-	count = 0;
-	infos->scene->collectible_count = 0;
-	i = 0;
-	while (++i < map_data.height - 1)
-	{
-		j = 0;
-		while (++j < map_data.width - 1)
-		{
-			if (is_collectible(map_data.map[i][j]))
-			{
-				if (count < 100)
-				{
-					infos->scene->collectible_positions[count][0] = j;
-					infos->scene->collectible_positions[count][1] = i;
-					count++;
-				}
-			}
-		}
-	}
-	infos->scene->collectible_count = count;
-}
-
-void	store_doors(t_map_data map_data, t_infos *infos)
-{
-	int	i;
-	int	j;
-	int	count;
-
-	count = 0;
-	infos->scene->door_count = 0;
-	i = 0;
-	while (++i < map_data.height - 1)
-	{
-		j = 0;
-		while (++j < map_data.width - 1)
-		{
-			if (is_door(map_data.map[i][j]))
-			{
-				if (count < 100)
-				{
-					infos->scene->door_positions[count][0] = j;
-					infos->scene->door_positions[count][1] = i;
-					infos->scene->door_positions[count][2] = 100;
-
-					count++;
-				}
-			}
-		}
-	}
-	infos->scene->door_count = count;
+	count = infos->scene->door_count;
+	if (count >= 100)
+		return ;
+	horizontal = (map_data.map[i][j - 1] == '1' && \
+				map_data.map[i][j + 1] == '1');
+	vertical = (map_data.map[i - 1][j] == '1' && \
+				map_data.map[i + 1][j] == '1');
+	infos->scene->door_positions[count][0] = j;
+	infos->scene->door_positions[count][1] = i;
+	infos->scene->door_positions[count][2] = 100;
+	if (horizontal && vertical)
+		infos->scene->door_positions[count][3] = ENCLOSED;
+	else if (vertical)
+		infos->scene->door_positions[count][3] = VERTICAL;
+	else
+		infos->scene->door_positions[count][3] = HORIZONTAL;
+	infos->scene->door_count++;
 }
 
 void	store_monsters(t_map_data map_data, t_infos *infos)
@@ -73,13 +41,12 @@ void	store_monsters(t_map_data map_data, t_infos *infos)
 		while (++j < map_data.width - 1)
 		{
 			if (is_monster(map_data.map[i][j]))
+				count++;
+			if (count > 1)
 			{
-				if (count < 100)
-				{
-					infos->scene->monster_positions[count][0] = j;
-					infos->scene->monster_positions[count][1] = i;
-					count++;
-				}
+				free_extended_map(map_data.map, map_data.height);
+				cleanup_parsing(infos);
+				exit_error(ERR_NB_MONSTER);
 			}
 		}
 	}
