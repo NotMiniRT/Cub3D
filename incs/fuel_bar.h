a15d0c4700b2ebd8ce4c6362c3b979be20b67aa3
Guillaume Ebersohl <guigui1991@live.fr>
2025-05-17 13:39:43 +0200

    style: changed fuel_bar comments

--- incs/fuel_bar.h
+++ incs/fuel_bar.h
@@ -4,15 +4,15 @@
 # include <stdbool.h>
 # include "structs.h"
 
-# define HUD_WIDTH 60           // Réduit la largeur (était 150)
-# define HUD_HEIGHT 450         // Ajusté également
-# define HUD_X 20               // Position X ajustée
-# define HUD_Y 420              // Position plus basse (était 120)
-# define GAUGE_WIDTH 30         // Jauge plus fine (était 40)
-# define GAUGE_HEIGHT 370       // Hauteur ajustée
+# define HUD_WIDTH 60           // Largeur de la fenetre
+# define HUD_HEIGHT 450         // Longueur de la fenetre
+# define HUD_X 20               // Position X de l'HUD
+# define HUD_Y 420              // Position Y de l'HUD
+# define GAUGE_WIDTH 30         // Largeur de la jauge
+# define GAUGE_HEIGHT 370       // Longueur de la jauge
 # define GAUGE_X 15             // Position X de la jauge centrée
 # define GAUGE_Y 65             // Position Y de la jauge dans le HUD
-# define GAUGE_BORDER 2			
+# define GAUGE_BORDER 2
 
 bool	display_hud(t_main_struct *main_struct);
 void	draw_fuel_gauge(t_image_cub *img, int fuel_level);
